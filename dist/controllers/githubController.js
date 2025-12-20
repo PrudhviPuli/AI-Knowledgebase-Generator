f21b915bee004f4968bdd6a1f5558bf2c41ebb25
PrudhviPuli <puliprudhvi.9@gmail.com>
2025-12-20 17:29:55 -0600

    Updated from JS to TS

---
+++ dist/controllers/githubController.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { exec } from "child_process";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const downloadController = (req, res) => {
+    const repoUrl = req.query.repo;
+    if (!repoUrl || typeof repoUrl !== 'string') {
+        return res.status(400).json({ message: 'repo query param required' });
+    }
+    const repoName = path.basename(repoUrl, ".git");
+    const destFolder = path.join(__dirname, "..", "downloaded_repos", repoName);
+    fs.mkdirSync(destFolder, { recursive: true });
+    exec(`git clone ${repoUrl} ${destFolder}`, (err) => {
+        if (err) {
+            return res.status(500).json({ message: err.message });
+        }
+        return res.status(200).json({ message: `Downloaded ${repoName}` });
+    });
+};
